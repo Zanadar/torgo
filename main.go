308c517b687aff7df6fc360b00387c0f7ef60734
Zander Mackie <zmackie@gmail.com>
2017-10-30 18:19:36 -0400

    Unmarshall into know struct

--- main.go
+++ main.go
@@ -4,19 +4,27 @@ import (
 	"crypto/sha1"
 	"flag"
 	"fmt"
-	"github.com/jackpal/bencode-go"
 	"net/http"
+	"net/url"
 	"os"
-	"sort"
+	"strconv"
+
+	"github.com/jackpal/bencode-go"
 )
 
 func init() {
 }
 
-type TorrentInfor struct {
-	Info map[string]interface{}
+type TorrentInfo struct {
+	Info
+	Announce string
+	Encoding string
 }
 type Info struct {
+	Name        string
+	Pieces      string
+	Length      int64
+	PieceLength int64 `bencode:"piece length"`
 }
 
 func main() {
@@ -30,34 +38,44 @@ func main() {
 
 	torrentBuf, err := os.Open(args[0])
 	if err != nil {
-		fmt.Printf("Problem with ", args[0])
+		fmt.Printf("Problem with ", args)
 	}
 
-	torrentParts, _ := bencode.Decode(torrentBuf)
+	torrentInfo := &TorrentInfo{}
+	err = bencode.Unmarshal(torrentBuf, torrentInfo)
+	if err != nil {
+		fmt.Printf("Problem unmarshalling ", err)
+	}
+	fmt.Printf("\n\n\ntorrentInfo: \n %#v \n\n", torrentInfo)
 
 	// We have to assert the type in order to work with it.
-	t := torrentParts.(map[string]interface{})
-	url := t["announce"].(string)
+
+	infoHash := sha1.New()
+	bencode.Marshal(infoHash, torrentInfo.Info)
+	fmt.Printf("Sha of inforString: % x", infoHash.Sum(nil))
+
+	url, err := url.Parse(torrentInfo.Announce)
+	if err != nil {
+		fmt.Printf("Errer: %e", err)
+	}
+	q := url.Query()
+	q.Add("info_hash", string(infoHash.Sum(nil)))
+	q.Add("peer_id", "boblog123")
+	q.Add("left", strconv.Itoa(int(torrentInfo.Info.Length)))
+	url.RawQuery = q.Encode()
 
 	//All of this monkey business is to sort the hash the info dictionay
 	// The keys have to appear in consistent order ....
-	info := t["info"].(map[string]interface{})
-	infoHash := sha1.New()
-	bencode.Marshal(infoHash, info)
-	fmt.Printf("Sha of inforString: % x", infoHash.Sum(nil))
 
-	resp, err := http.Get(url)
+	resp, err := http.Get(url.String())
 	if err != nil {
-		fmt.Printf("Errer:", err)
+		fmt.Printf("Errer: %e", err)
 	} else {
-		fmt.Println(resp)
+		fmt.Printf("\n\n\nUrl %+v %#v", url, url)
+		fmt.Printf("\n\n\nResponse %+v %#v", resp, resp)
 	}
 
 	//Feedback
 	if *verbose {
-		for k, v := range info {
-			fmt.Printf("%s: %q\n\n", k, v)
-		}
 	}
-
 }
